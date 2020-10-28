65c838608ae043809cd9d7cd4276711fc34420ad
Yehuda Bortz <yehudabortz@icloud.com>
2020-10-28 15:11:12 -0500

    commit tree 62cf029efe941e355bee32c749c44c3132f3b84c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarves)
+    dwarves.each_with_index do |n, i|
+      i += 1
+      puts "#{i}. #{n}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map {|n| n.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+ short_words.any? {|n| n.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.detect do |n|
+  cheese_types.include?(n)
+  end
 end
+
+# def find_the_cheese(snacks)
+#   includes_cheese = []
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   snacks.select do |n|
+#   includes_cheese << cheese_types.include?(n)
+#   end
+#   includes_cheese.join(" ")
+# end
